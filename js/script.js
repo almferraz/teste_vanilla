1d60cdf350c161a611f228f06d763d74d468bb99
André Ferraz <almferraz@gmail.com>
2021-08-06 15:07:58 -0300

    tentativa de escrever o JS

--- js/script.js
+++ js/script.js
@@ -4,28 +4,20 @@ const cartImg = document.querySelector('.cartImg');
 vitrineApi
   .then((r) => r.json())
   .then((lista) => {
-    // console.log(lista[0].imageUrl);
-    // cartImg.textContent(lista[0].imageUrl);
-    // lista.map((produtos) => {
-    //   console.log(produtos);
-    // });
+    console.log(lista[0].imageUrl);
+    cartImg.textContent(lista[0].imageUrl);
+    lista.map((produtos) => {
+      console.log(produtos);
+    });
   });
 
 /* -------------------------------*/
-//   const vitrineApi = fetch('https://corebiz-test.herokuapp.com/api/v1/products');
-
-// vitrineApi
-//   .then((r) => r.json())
-//   .then((lista) => {
-//     lista.map((produtos) => {
-//       console.log(produtos);
-//     });
-//   });
 
 /*****LOCAL STORAGE *********/
 
 newslatter = () => {
   const news = document.getElementById('newslatter');
+
   news.addEventListener('submit', (e) => {
     e.preventDefault();
     const nome = document.getElementById('nome').value;
